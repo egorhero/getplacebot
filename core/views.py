c26431363d46c8b079ee17edc2b5ce4077098d60
Egor Richman <egor.richman@gmail.com>
2020-09-28 20:38:25 +0700

    x

--- core/views.py
+++ core/views.py
@@ -8,6 +8,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 import datetime
+import io
 
 
 ##########
